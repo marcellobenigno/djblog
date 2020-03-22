5cd0d2e49fbfd8cc463e387a721094236c0807b3
marcello_benigno <benigno.marcello@gmail.com>
2020-03-22 08:59:59 -0300

    refatorando e adicionando o primeiro teste

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # djblog
 Um blog criado com python e django
+
+
+## Como desenvolver?
+
+1. Clone o repositório.
+2. Crie um virtualenv com Python 3.8.0
+3. Ative o virtualenv.
+4. Instale as dependências.
+5. Configure o .env
+6. Execute os testes.
+
+```console
+git clone git@github.com:marcellobenigno/djblog.git
+cd djblog
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements-dev.txt
+cp contrib/env-sample .env
+python manage.py test
+```
---
+++ djblog/core/tests/test_view_index.py
@@ -0,0 +1,19 @@
+from django.shortcuts import resolve_url as r
+from django.test import TestCase
+
+
+class TestViewIndex(TestCase):
+    def setUp(self):
+        self.response = self.client.get(r('core:index'))
+
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'core/index.html')
+
+    def test_admin_link(self):
+        expected = 'href="{}"'.format(r('/admin/'))
+        self.assertContains(self.response, expected)
