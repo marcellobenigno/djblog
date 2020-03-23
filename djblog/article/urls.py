eeb324038ff22a4f47c5d3c680fb2ff4417353f1
marcello_benigno <benigno.marcello@gmail.com>
2020-03-23 20:03:42 -0300

    muda o nome da url

--- djblog/article/urls.py
+++ djblog/article/urls.py
@@ -6,7 +6,7 @@ app_name = 'article'
 
 urlpatterns = [
     path('', v.article_list, name='article_list'),
-    path('painel/', v.article_admin_list, name='article_admin_list'),
+    path('gerenciar/', v.article_admin_list, name='article_admin_list'),
     path('nova/', v.article_create, name='article_create'),
     path('<slug:slug>/editar/', v.article_update, name='article_update'),
     path('<slug:slug>/', v.article_detail, name='article_detail'),
